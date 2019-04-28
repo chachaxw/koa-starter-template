070a6cd3c6c1b126bb9e2c8454b91a6c311e8218
chacha <867571123@qq.com>
2019-04-28 10:30:11 +0800

    Add static folder config

--- server/app.js
+++ server/app.js
@@ -3,11 +3,14 @@ import Router from 'koa-router';
 import path from 'path';
 import views from 'koa-views';
 import bodyParser from 'koa-bodyparser';
+import koaStatic from 'koa-static';
 
 import HomeController from './controller';
 
 const app = new Koa();
 const router = new Router();
+// 静态资源目录对于相对入口文件index.js的路径
+const staticPath = '../public';
 
 router.get('/', HomeController);
 
@@ -15,7 +18,9 @@ app.use(bodyParser());
 
 app.use(views(path.join(__dirname, './views'), { extension: 'pug' }));
 
-app.use(router.routes()).use(router.allowedMethods())
+app.use(router.routes()).use(router.allowedMethods());
+
+app.use(koaStatic(path.join( __dirname,  staticPath)));
 
 app.listen(3000, () => {
   console.log('App is starting at port 3000');
