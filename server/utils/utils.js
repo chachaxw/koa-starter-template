2291cde734024d74c60e240f4467328b94cf424c
chacha <867571123@qq.com>
2019-05-08 21:45:44 +0800

    Update config

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -2,6 +2,6 @@
  * 判断对象或者数组是否为空
  * @param obj
  */
-export function isEmpty(obj: any): boolean {
+export function isEmpty(obj) {
   return [Object, Array].includes((obj || {}).constructor) && !Object.entries((obj || {})).length;
 }
